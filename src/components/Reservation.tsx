c4eccb3e34a12ed58975ab0774d8341ccfce55f0
ZanderMC <ZanderMC @users.noreply.github.com>
2026-04-04 23:08:24 -0500

    add calendar

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,8 +1,23 @@
+'use client'
+import { Calendar } from "@/ui/Calendar";
+import { useState } from "react";
 
 export function Reservation(){
+     const getStartOfToday = () => {
+        const now = new Date()
+        return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    }
+      const [selectedDate, setSelectedDate] = useState<Date | null>(() => getStartOfToday())
+    const [selectedSlots, setSelectedSlots] = useState<string[]>([])
     return(
         <section className="bg-red-200 h-screen w-full">
- 
+      <Calendar
+                    selectedDate={ selectedDate}
+                    selectedSlots={selectedSlots}
+                    courtName={"wa"}
+                    onSelectedDateChange={setSelectedDate}
+                    onSelectedSlotsChange={setSelectedSlots}
+                />
         </section>
     )
 }
